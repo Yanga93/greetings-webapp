1c03fd4a1c8b34724e576135b38eb38fa66004a1
Yanga93 <yangazukelwa@gmail.com>
2017-08-02 10:18:42 +0200

    new updates

--- index.js
+++ index.js
@@ -17,20 +17,22 @@ app.get('/greeted', function(req, res){
 // create a route for counter
 app.get('/counter/:username', function(req, res){
   var namesMap = {};
+  var timesGreeted = 0;
     for (var i = 0; i < greeted.length; i++) {
       var namesGreeted = greeted[i];
-      namesMap[namesGreeted] = namesMap[namesGreeted] ? namesMap[namesGreeted] + 1:1;
+      if(namesMap[namesGreeted] !== undefined) {
+        var increment = namesMap[namesGreeted] ? namesMap[namesGreeted] + 1:1;
+        namesMap[namesGreeted] = increment;
+        timesGreeted = namesMap[namesGreeted];
+      }else {
+        namesMap[namesGreeted] = 1;
+      }
     }
-    res.send("Hello, " + req.params.username + " has been greeted " + namesMap[namesGreeted] + " times.");
+    res.send("Hello, " + req.params.username + " has been greeted " + timesGreeted + " times.");
     // Hello, <USER_NAME> has been greeted <COUNTER> times
 });
 
 //start the server
 var index = app.listen(3000, function () {
-
- // var host = index.address().address;
- // var port = index.address().port;
-
- // console.log('Example app listening at http://%s:%s', host, port);
 
 });
