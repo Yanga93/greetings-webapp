b28bacfa8e38e0eaaf8bc2d38cbd0c896d3996c1
Yanga93 <yangazukelwa@gmail.com>
2017-08-18 12:10:03 +0200

    installed mongoose and set mocha function and tested it if its working

---
+++ test/models.test.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+describe('models should be able to', function(){
+  it('store greetedNames to MongoDB', function(){
+    // assert.equal('Yanga','Yanga')
+  });
+});
