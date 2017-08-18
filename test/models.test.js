472329beee8c7662eb14f26dfb33d1f835c11d10
Yanga93 <yangazukelwa@gmail.com>
2017-08-18 17:57:40 +0200

    created factory test function

--- test/models.test.js
+++ test/models.test.js
@@ -1,6 +1,17 @@
 const assert = require('assert');
-describe('models should be able to', function(){
-  it('store greetedNames to MongoDB', function(){
+const Models = require('../models');
+describe('models should be able to', function() {
+
+  it('store greetedNames to MongoDB', function() {
+
+    var models = Models("mongodb://localhost/names");
+
+    models.greetedNames
+      .create({ name: 'The test name'}, function (err) {
+        done(err);
+
+      })
+
     // assert.equal('Yanga','Yanga')
   });
 });
