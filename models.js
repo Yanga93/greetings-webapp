c342daab7999b12d7ae6e259445d7f5d857fd439
Yanga93 <yangazukelwa@gmail.com>
2017-08-31 16:01:59 +0200

    configured remote for heroku

--- models.js
+++ models.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 module.exports = function(mongoUrl) {
+const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/greet-me";
   mongoose.connect(mongoUrl);
 
   const Person = mongoose.model('person', {
